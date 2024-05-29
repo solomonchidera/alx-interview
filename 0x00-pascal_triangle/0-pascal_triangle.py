c0c6a085504a5c7f89c90b572e76a2426efb3ea9
solomonchidera <solochidera10@gmail.com>
2024-05-29 17:15:08 +0100

    pascal triangle algorithm

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!//usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
+def pascal_triangle(n):
+    triangle = []
+    
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+        triangle.append(row)
+    
+    return triangle
