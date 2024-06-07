e16f157e488875ff6d581543ed860f349b9371a3
solomonchidera <solochidera10@gmail.com>
2024-06-07 15:28:50 +0100

    lockbox interview question using graph and set

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    # Number of boxes
+    n = len(boxes)
+
+    # Set to keep track of unlocked boxes
+    unlocked = set()
+    unlocked.add(0)  # First box is unlocked
+
+    # Stack to keep track of boxes to check
+    stack = [0]  # Start with the first box
+
+    # Traverse boxes
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < n and key not in unlocked:
+                unlocked.add(key)
+                stack.append(key)
+
+    # Check if all boxes are unlocked
+    return len(unlocked) == n
