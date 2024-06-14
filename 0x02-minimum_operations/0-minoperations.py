ace82c0edee7ef2234757c2aa86f9f531b9f702a
solomonchidera <solochidera10@gmail.com>
2024-06-14 04:54:12 +0100

    copyall and paste

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+"""
+Module Docs
+"""
+
+
+def minOperations(n):
+    """
+    Function Docs
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
