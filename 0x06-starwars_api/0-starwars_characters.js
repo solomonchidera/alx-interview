9c974d2a44ecca73cee626d7deb903fd23d7cec0
solomonchidera <solochidera10@gmail.com>
2024-07-12 06:02:57 +0100

    star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const util = require('util');
+const filmId = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const film = (await prequest(URL, { json: true })).body;
+    for (const url of film.characters) {
+      const character = (await prequest(url, { json: true })).body;
+      console.log(character.name);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
