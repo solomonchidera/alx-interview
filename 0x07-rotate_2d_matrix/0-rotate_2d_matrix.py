bc6af263dbe2b1d3a5d58816241671a27d529bf0
solomonchidera <solochidera10@gmail.com>
2024-07-18 22:01:24 +0100

    rotate the matrix or escape it

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -14,7 +14,7 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Step 2: Reverse each row
     for i in range(n):
         matrix[i].reverse()
